e6f602fce67699e2e2b0efcb9ca16b13ddf83026
Anokhin Alexey <exile.developer@gmail.com>
2020-09-28 08:51:20 +0300

    Issue 5102 Converted ProductLoadAllPaged to LINQ

--- src/Presentation/Nop.Web/Controllers/ProductController.cs
+++ src/Presentation/Nop.Web/Controllers/ProductController.cs
@@ -285,15 +285,9 @@ namespace Nop.Web.Controllers
             if (!_catalogSettings.NewProductsEnabled)
                 return Content("");
 
-            var products = _productService.SearchProducts(
-                storeId: _storeContext.CurrentStore.Id,
-                visibleIndividuallyOnly: true,
-                markedAsNewOnly: true,
-                orderBy: ProductSortingEnum.CreatedOn,
-                pageSize: _catalogSettings.NewProductsNumber);
-
-            var model = new List<ProductOverviewModel>();
-            model.AddRange(_productModelFactory.PrepareProductOverviewModels(products));
+            var model = _productModelFactory
+                .PrepareProductOverviewModels(_productService.GetProductsMarkedAsNew(_storeContext.CurrentStore.Id))
+                .ToList();
 
             return View(model);
         }
@@ -311,12 +305,8 @@ namespace Nop.Web.Controllers
 
             var items = new List<RssItem>();
 
-            var products = _productService.SearchProducts(
-                storeId: _storeContext.CurrentStore.Id,
-                visibleIndividuallyOnly: true,
-                markedAsNewOnly: true,
-                orderBy: ProductSortingEnum.CreatedOn,
-                pageSize: _catalogSettings.NewProductsNumber);
+            var products = _productService.GetProductsMarkedAsNew(_storeContext.CurrentStore.Id);
+            
             foreach (var product in products)
             {
                 var productUrl = Url.RouteUrl("Product", new { SeName = _urlRecordService.GetSeName(product) }, _webHelper.CurrentRequestProtocol);
